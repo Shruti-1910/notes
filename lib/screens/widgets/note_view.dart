458ce16887d4e0f462ecfcd5634590bfd14ee68e
Shruti-1910 <skshrutikumari.46@gmail.com>
2024-05-29 19:38:08 +0530

    [#10]: Implemented Deletion Feature

--- lib/screens/widgets/note_view.dart
+++ lib/screens/widgets/note_view.dart
@@ -2,16 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:notes/models/note_model.dart';
 
 class NoteView extends StatelessWidget {
-  const NoteView({super.key, required this.note, required this.index});
+  const NoteView(
+      {super.key,
+      required this.note,
+      required this.index,
+      required this.onNoteDeleted});
 
   final Note note;
   final int index;
+
+  final Function(int) onNoteDeleted;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Note View"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Delete This Note?"),
+                      content: Text("Note ${note.title} will be deleted!"),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                            onNoteDeleted(index);
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text("DELETE"),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text("CANCEL"),
+                        ),
+                      ],
+                    );
+                  });
+            },
+            icon: Icon(Icons.delete),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(10),
